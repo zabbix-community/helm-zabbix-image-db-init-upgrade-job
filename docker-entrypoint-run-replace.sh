eb529b38e648cc0ddc1598b4e3c97ef04e4223b7
Christian Anton <christian.anton@inqbeo.de>
2024-12-30 22:27:25 +0100

    migrate from Gitlab to Github, new upgrade/install logic
    This new version of the image will handle install and upgrade actions
    differently, so it will not need a serviceAccount being able to access
    Kubernetes API when doing a plain install.
    
    With these changes, it is now possible to use this image as a
    pre-install script, solving problems with `helm install/upgrade --wait`
    and ArgoCD.

--- docker-entrypoint-run-replace.sh
+++ docker-entrypoint-run-replace.sh
@@ -1,6 +1,13 @@
 
 init_and_upgrade_db() {
     # get version of db schema and version of zabbix server within this image
+    HELM_HOOK_TYPE="${HELM_HOOK_TYPE:-unknown}"
+    echo "job hook type is ${HELM_HOOK_TYPE}"
+
+    # at this point, we know that Zabbix DB schema is existing (either created or verified to be existing in the steps
+    # before), and we assume that the version of zabbix_server binary coming with this image is the version of Zabbix
+    # server that will be run, afterwards. Therefore, we first find out major releases of the database schema and of
+    # the zabbix_server binary inside this image.
     db_version=$(psql_query "SELECT mandatory FROM ${DB_SERVER_SCHEMA}.dbversion" "${DB_SERVER_DBNAME}")
     echo "DB version found: ${db_version} in database ${DB_SERVER_DBNAME} and user ${DB_SERVER_ROOT_USER} on host ${DB_SERVER_HOST}"
     db_version_major=${db_version:0:4}
@@ -9,32 +16,39 @@ init_and_upgrade_db() {
 
 
     # compare those and figure whether a major release upgrade is necessary
-    if [ ${zbx_version_major} -gt ${db_version_major} ]; then
-
+    if [[ $zbx_version_major -gt $db_version_major ]]; then
         echo "** initializing the major release upgrade process"
         # in case of an upgrade, it doesn't matter if we come from an HA-enabled or a single-node setup. We will after preparation start a single-node
         # pod anyway which will not fail if it finds an entry of a non-HA-enabled one in the database.
 
+	
         # scale down existing Zabbix deployment
-        deployment_replicas=$(kubectl get deploy ${ZBX_SERVER_DEPLOYMENT_NAME} -o jsonpath='{.spec.replicas}')
-        echo "** scaling zabbix server deployment with name ${ZBX_SERVER_DEPLOYMENT_NAME} from ${deployment_replicas} to 0 replicas"
-        kubectl scale deploy ${ZBX_SERVER_DEPLOYMENT_NAME} --replicas=0
+	# This is actually only needed when this job has been started as a pre-UPGRADE job, as otherwise there would be
+	# no running pods accessing the database
+	if [[ $HELM_HOOK_TYPE == *"upgrade" ]]; then
+		deployment_replicas=$(kubectl get deploy ${ZBX_SERVER_DEPLOYMENT_NAME} -o jsonpath='{.spec.replicas}')
+        	echo "** scaling zabbix server deployment with name ${ZBX_SERVER_DEPLOYMENT_NAME} from ${deployment_replicas} to 0 replicas"
+        	kubectl scale deploy ${ZBX_SERVER_DEPLOYMENT_NAME} --replicas=0
 
-        # wait for no active zabbix_servers speaking with the db anymore
-        WAIT_TIMEOUT=1
-        while true :
-        do
-            active_servers=$(psql_query "SELECT COUNT(*) FROM ${DB_SERVER_SCHEMA}.ha_node WHERE lastaccess >= extract(epoch from now()) - 10" "${DB_SERVER_DBNAME}")
-            deployment_pods=$(kubectl get pods -l app=${ZBX_SERVER_DEPLOYMENT_NAME} -o custom-columns=NAME:.metadata.name --no-headers | wc -l)
-            if [ ${active_servers} -eq 0 -a ${deployment_pods} -eq 0 ]; then
-                break
-            fi
-            echo "**** ${active_servers} active zabbix_server instances seen within less than 10 seconds and ${deployment_pods} pods of deployment ${ZBX_SERVER_DEPLOYMENT_NAME} still running... waiting"
-            sleep $WAIT_TIMEOUT
-        done
+        	# wait for no active zabbix_servers speaking with the db anymore
+        	WAIT_TIMEOUT=1
+        	while true :
+        	do
+        		active_servers=$(psql_query "SELECT COUNT(*) FROM ${DB_SERVER_SCHEMA}.ha_node WHERE lastaccess >= extract(epoch from now()) - 10" "${DB_SERVER_DBNAME}")
+        		deployment_pods=$(kubectl get pods -l app=${ZBX_SERVER_DEPLOYMENT_NAME} -o custom-columns=NAME:.metadata.name --no-headers | wc -l)
+        		if [[ $active_servers -eq 0 && $deployment_pods -eq 0 ]]; then
+        			break
+        		fi
+        		echo "**** ${active_servers} active zabbix_server instances seen within less than 10 seconds and ${deployment_pods} pods of deployment ${ZBX_SERVER_DEPLOYMENT_NAME} still running... waiting"
+        		sleep $WAIT_TIMEOUT
+		done
+	fi
+
 
         # now we start the zabbix_server binary with the configuration created above, and we halt it as soon as
-        # it went over the part when it upgrades the database schema
+        # it went over the point when it upgraded the database schema. This is because there is no controlled
+	# way in the zabbix_server binary (yet) to only upgrade the DB schema and exit gracefully:
+	# https://support.zabbix.com/browse/ZBXNEXT-9453
         PIPE="/tmp/zabbix_output_pipe"
         mkfifo "$PIPE"
         /usr/sbin/zabbix_server --foreground -c /etc/zabbix/zabbix_server.conf > "$PIPE" 2>&1 &
@@ -43,7 +57,7 @@ init_and_upgrade_db() {
             echo "$line"
 
             # Check if the line contains the string "starting HA manager"
-            if [[ "$line" == *"starting HA manager"* ]]; then
+            if [[ $line == *"starting HA manager"* ]]; then
                 echo "Found 'starting HA manager' - killing process"
                 kill "$ZABBIX_PID"
                 break
@@ -58,67 +72,42 @@ init_and_upgrade_db() {
         while true :
         do
             active_servers=$(psql_query "SELECT COUNT(*) FROM ${DB_SERVER_SCHEMA}.ha_node WHERE lastaccess >= extract(epoch from now()) - 10 and status in (0, 3)" "${DB_SERVER_DBNAME}")
-            if [ ${active_servers} -eq 0 ]; then
+            if [[ $active_servers -eq 0 ]]; then
                 break
             fi
             echo "**** ${active_servers} active zabbix_server instances seen within less than 10 seconds... waiting"
             sleep $WAIT_TIMEOUT
         done
 
-        # delete remaining active standalone server entries from ha_node table in order that the ha-enabled ones can start up
-        # otherwise they will most probably fail for >1 minute
+        # delete eventually remaining active standalone server entries from ha_node table in order that the ha-enabled
+	# ones can start up otherwise they will most probably fail for >1 minute
         psql_query "DELETE FROM ${DB_SERVER_SCHEMA}.ha_node WHERE name='' and status=3" "${DB_SERVER_DBNAME}"
-
-        # scale up the deployment again
-        echo "** scaling up zabbix server deployment with name ${ZBX_SERVER_DEPLOYMENT_NAME} to ${deployment_replicas} replicas"
-        kubectl scale deploy ${ZBX_SERVER_DEPLOYMENT_NAME} --replicas=${deployment_replicas}
 
         # we are ready to go
         exit 0
 
 
-    elif [ ${zbx_version_major} -lt ${db_version_major} ]; then
+    elif [[ $zbx_version_major -lt $db_version_major ]]; then
         echo "*** FATAL database schema version ${db_version_major} is higher than zabbix server's ${zbx_version_major}, downgrade is not supported!"
         exit 252
 
-
     else
         echo "*** DB schema is up-to-date, checking for whether we come from a non-HA enabled setup"
-        # check if we are coming from a standalone setup, which means, there are entries in the ha_node table with no name set
-        # In that case, we need to downscale the deployment, doesn't matter whether this is an upgrade or not, and then remove
-        # the entries mapping to these server nodes from the ha_nodes table.
-        # Reason to do this is that Zabbix Server in HA mode refuses to start if a non-HA node has been seen within the FAILOVER
-        # PERIOD which is 1 minute by default
-        echo "** checking for eventually yet connected active non-HA mode pods of Zabbix Server..."
+        # check if we are coming from a standalone setup, which means, there are entries in the ha_node table with no
+	# name set. In that case, we need to wait for those to go away
+        # Reason to do this is that Zabbix Server in HA mode refuses to start if a non-HA node has been seen within the
+	# FAILOVER PERIOD which is 1 minute by default
+        echo "** checking for eventually still connected active non-HA mode pods of Zabbix Server..."
         while true :
         do
-            active_servers=$(psql_query "SELECT COUNT(*) FROM ${DB_SERVER_SCHEMA}.ha_node WHERE lastaccess >= extract(epoch from now()) - 60 AND status=3 and name=''" "${DB_SERVER_DBNAME}")
-            if [ ${active_servers} -eq 0 ]; then
-                echo "*** none found, continuing"
-                break
-            fi
+		active_servers=$(psql_query "SELECT COUNT(*) FROM ${DB_SERVER_SCHEMA}.ha_node WHERE lastaccess >= extract(epoch from now()) - 60 AND status=3 and name=''" "${DB_SERVER_DBNAME}")
+        	if [[ $active_servers -eq 0 ]]; then
+                	echo "*** none found, continuing"
+                	break
+        	fi
 
-            echo "*** found ${active_servers} in db, scaling down deployment before continuation"
-            deployment_replicas=$(kubectl get deploy ${ZBX_SERVER_DEPLOYMENT_NAME} -o jsonpath='{.spec.replicas}')
-            kubectl scale deploy ${ZBX_SERVER_DEPLOYMENT_NAME} --replicas=0
-
-            # wait for pods to disappear
-            while true :
-            do
-                deployment_pods=$(kubectl get pods -l app=${ZBX_SERVER_DEPLOYMENT_NAME} -o custom-columns=NAME:.metadata.name --no-headers | wc -l)
-                if [ ${deployment_pods} -eq 0 ]; then
-                    break
-                fi
-                echo "**** ${deployment_pods} pods of deployment ${ZBX_SERVER_DEPLOYMENT_NAME} still running... waiting"
+        	echo "*** found ${active_servers} in db, waiting 10 seconds"
                 sleep $WAIT_TIMEOUT
-            done
-
-            # delete entries from db table
-	    echo "*** deleting non-HA enabled nodes from ha_nodes table"
-            psql_query "DELETE FROM ${DB_SERVER_SCHEMA}.ha_node WHERE name='' and status=3" "${DB_SERVER_DBNAME}"
-
-	    echo "*** scale deployment ${ZBX_SERVER_DEPLOYMENT_NAME} to ${deployment_replicas}"
-            kubectl scale deploy ${ZBX_SERVER_DEPLOYMENT_NAME} --replicas=${deployment_replicas}
         done
         exit 0
     fi
